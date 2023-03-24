b2a55b1c2fe3365e38e52b0f4d16ac19a885858a
lusianasafrida <111295850+lusianasafrida@users.noreply.github.com>
2023-03-24 11:42:52 +0700

    Add files via upload

---
+++ src/dist/js/script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector('.menu');
+const btnBTT = document.querySelector('.fa-circle-arrow-up');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("is-active");
+
+    menu.classList.toggle("menu-active");
+});
+
+window.addEventListener('scroll', () => {
+    windowPosition = window.scrollY > 300;
+    btnBTT.classList.toggle("btn-active", windowPosition);
+})
